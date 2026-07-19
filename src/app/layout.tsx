5a5cee50a0e5364bba376705f61f88f8157566fc
SRManagement <ryanbellmanagement@gmail.com>
2026-07-18 22:56:43 -0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Playfair_Display } from "next/font/google";
+import { Playfair_Display, Anton } from "next/font/google";
 import "./globals.css";
 
 const playfair = Playfair_Display({
@@ -7,6 +7,12 @@ const playfair = Playfair_Display({
   subsets: ["latin"],
   style: ["italic", "normal"],
   weight: ["600", "700", "800", "900"],
+});
+
+const anton = Anton({
+  variable: "--font-condensed",
+  subsets: ["latin"],
+  weight: ["400"],
 });
 
 export const metadata: Metadata = {
@@ -20,7 +26,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${playfair.variable} h-full antialiased`}>
+    <html
+      lang="en"
+      className={`${playfair.variable} ${anton.variable} h-full antialiased`}
+    >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
